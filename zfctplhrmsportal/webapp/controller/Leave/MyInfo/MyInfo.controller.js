53d87a8612603539a9c899a97752c30d4f5cdc14
Joel Dmello <joel.dmello@castaliaz.co.in>
2026-04-13 11:55:12 +0000

    9th push

--- zfctplhrmsportal/webapp/controller/Leave/MyInfo/MyInfo.controller.js
+++ zfctplhrmsportal/webapp/controller/Leave/MyInfo/MyInfo.controller.js
@@ -1,10 +1,46 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/BusyIndicator"
+], (Controller,MessageToast,BusyIndicator) => {
     "use strict";
 
     return Controller.extend("zfctplhrmsportal.controller.Leave.MyInfo.MyInfo", {
         onInit() {
+           
+        },
+
+        onViewPress(){
+            var sEmployee = this.byId("idEmployee").getValue();
+
+            if(sEmployee){
+                MessageToast.show("Please enter Employee ID");
+                return;
+            }
+             
+            var sPath = "/Orders(" +sEmployee+ ")";
+            this.getView().bindElement({
+                path: sPath,
+                parameters: {
+                    expand: "Order_Details"
+                },
+                events:{
+                    dataRequested(){
+                        BusyIndicator.show(0);
+                    },
+                    dataRecived(){
+                        BusyIndicator.hide();
+                    }
+                }
+            })
+        },
+
+        onResetPress(){
+            this.byId("idEmployee").setValue();
+            this.byId("idPeriod").setValue();
+            this.byId("idLeaveType").setValue();
+
+            this.getView().unbindElement();
         }
     });
 });
